4aa7a1a8a2e9666043ef0107f7f6b0984acf592f
Guillaume Pastor <guillaume.pastor@dont-nod.com>
2019-05-15 11:17:53 +0200

    Camera renaming.

--- Exp-Engine/Camera/Camera.cpp
+++ Exp-Engine/Camera/Camera.cpp
@@ -3,81 +3,81 @@
 namespace Exp
 {
 	Camera::Camera(glm::vec3 position, glm::vec3 forward, glm::vec3 up) :
-		_position(position),
-		_forward(forward),
-		_up(up)
+		m_position(position),
+		m_forward(forward),
+		m_up(up)
 
 	{
-		_worldUp = _up;
-		_targetPosition = _position;
-		_targetYaw = _yaw;
-		_targetPitch = _pitch;
+		m_worldUp = m_up;
+		m_targetPosition = m_position;
+		m_targetYaw = m_yaw;
+		m_targetPitch = m_pitch;
 
 
-		updateView();
+		UpdateView();
 
-		setPerspective(_fovY, _aspectRatio, _nearPlane, _farPlane);
+		SetPerspective(m_fovY, m_aspectRatio, m_nearPlane, m_farPlane);
 	}
 
-	void Camera::update(float deltatime)
+	void Camera::Update(float deltatime)
 	{
-		_position = glm::lerp(_position, _targetPosition, glm::clamp(1.f - pow(1.f - _damping / 60.f, 60.f * deltatime), 0.0f, 1.0f));
-		_yaw = glm::lerp(_yaw, _targetYaw, glm::clamp(1.f - pow(1.f - _damping * 2.0f / 60.f, 60.f * deltatime), 0.0f, 1.0f));
-		_pitch = glm::lerp(_pitch, _targetPitch, glm::clamp(1.f - pow(1.f - _damping * 2.0f / 60.f, 60.f * deltatime), 0.0f, 1.0f));
+		m_position = glm::lerp(m_position, m_targetPosition, glm::clamp(1.f - pow(1.f - m_damping / 60.f, 60.f * deltatime), 0.0f, 1.0f));
+		m_yaw = glm::lerp(m_yaw, m_targetYaw, glm::clamp(1.f - pow(1.f - m_damping * 2.0f / 60.f, 60.f * deltatime), 0.0f, 1.0f));
+		m_pitch = glm::lerp(m_pitch, m_targetPitch, glm::clamp(1.f - pow(1.f - m_damping * 2.0f / 60.f, 60.f * deltatime), 0.0f, 1.0f));
 
 		glm::vec3 newForward;
-		newForward.x = float(cos(0.0174533 * _pitch) * cos(0.0174533 * _yaw));
-		newForward.y = float(sin(0.0174533 * _pitch));
-		newForward.z = float(cos(0.0174533 * _pitch) * sin(0.0174533 * _yaw));
-		_forward = glm::normalize(newForward);
-		_right = glm::normalize(glm::cross(_forward, _worldUp));
-		_up = glm::cross(_right, _forward);
+		newForward.x = float(cos(0.0174533 * m_pitch) * cos(0.0174533 * m_yaw));
+		newForward.y = float(sin(0.0174533 * m_pitch));
+		newForward.z = float(cos(0.0174533 * m_pitch) * sin(0.0174533 * m_yaw));
+		m_forward = glm::normalize(newForward);
+		m_right = glm::normalize(glm::cross(m_forward, m_worldUp));
+		m_up = glm::cross(m_right, m_forward);
 
-		updateView();
+		UpdateView();
 	}
 
-	void Camera::updateMouse(float deltaX, float deltaY)
+	void Camera::UpdateMouse(float deltaX, float deltaY)
 	{
-		if (_disableMouse)
+		if (m_disableMouse)
 			return;
 
-		float x = deltaX * _sensitivity;
-		float y = deltaY * _sensitivity;
+		float x = deltaX * m_sensitivity;
+		float y = deltaY * m_sensitivity;
 
-		_targetYaw += x;
-		_targetPitch += y;
+		m_targetYaw += x;
+		m_targetPitch += y;
 
-		if (_targetYaw == 0.0f) _targetYaw = 0.01f;
-		if (_targetPitch == 0.0f) _targetPitch = 0.01f;
+		if (m_targetYaw == 0.0f) m_targetYaw = 0.01f;
+		if (m_targetPitch == 0.0f) m_targetPitch = 0.01f;
 
-		if (_targetPitch > 89.0f)  _targetPitch = 89.0f;
-		if (_targetPitch < -89.0f) _targetPitch = -89.0f;
+		if (m_targetPitch > 89.0f)  m_targetPitch = 89.0f;
+		if (m_targetPitch < -89.0f) m_targetPitch = -89.0f;
 	}
 
-	void Camera::updateKey(float deltaTime, CameraMovement direction)
+	void Camera::UpdateKey(float deltaTime, CameraMovement direction)
 	{
-		float speed = _speed * deltaTime;
+		float speed = m_speed * deltaTime;
 		if (direction == CameraMovement::FORWARD)
-			_targetPosition = _targetPosition + _forward * speed;
+			m_targetPosition = m_targetPosition + m_forward * speed;
 		else if (direction == CameraMovement::BACKWARD)
-			_targetPosition = _targetPosition - _forward * speed;
+			m_targetPosition = m_targetPosition - m_forward * speed;
 		else if (direction == CameraMovement::LEFT)
-			_targetPosition = _targetPosition - _right * speed;
+			m_targetPosition = m_targetPosition - m_right * speed;
 		else if (direction == CameraMovement::RIGHT)
-			_targetPosition = _targetPosition + _right * speed;
+			m_targetPosition = m_targetPosition + m_right * speed;
 		else if (direction == CameraMovement::UP)
-			_targetPosition = _targetPosition + _worldUp * speed;
+			m_targetPosition = m_targetPosition + m_worldUp * speed;
 		else if (direction == CameraMovement::DOWN)
-			_targetPosition = _targetPosition - _worldUp * speed;
+			m_targetPosition = m_targetPosition - m_worldUp * speed;
 	}
 
-	void Camera::updateView()
+	void Camera::UpdateView()
 	{
-		_view = lookAt(_position, _position + _forward, _up);
-		_viewProjection = _projection * _view;
+		m_view = LookAt(m_position, m_position + m_forward, m_up);
+		m_viewProjection = m_projection * m_view;
 	}
 
-	glm::mat4 Camera::lookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
+	glm::mat4 Camera::LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
 	{
 		//Compute new axis
 		glm::vec3 zAxis = glm::normalize(position - target);
@@ -105,13 +105,13 @@ namespace Exp
 		return rotation * translation;
 	}
 
-	void Camera::setPerspective(float fovY, float aspectRatio, float nearPlane, float farPlane)
+	void Camera::SetPerspective(float fovY, float aspectRatio, float nearPlane, float farPlane)
 	{
-		_fovY = fovY;
-		_aspectRatio = aspectRatio;
-		_nearPlane = nearPlane;
-		_farPlane = farPlane;
-		_projection = glm::perspective(glm::radians(_fovY), _aspectRatio, _nearPlane, _farPlane);
+		m_fovY = fovY;
+		m_aspectRatio = aspectRatio;
+		m_nearPlane = nearPlane;
+		m_farPlane = farPlane;
+		m_projection = glm::perspective(glm::radians(m_fovY), m_aspectRatio, m_nearPlane, m_farPlane);
 	}
 
 
--- Exp-Engine/Camera/Camera.h
+++ Exp-Engine/Camera/Camera.h
@@ -19,54 +19,54 @@ namespace Exp
 	class Camera
 	{
 	public:
-		glm::mat4 _view = glm::mat4(1.0f);
-		glm::mat4 _projection = glm::mat4(1.0f);
+		glm::mat4 m_view = glm::mat4(1.0f);
+		glm::mat4 m_projection = glm::mat4(1.0f);
 
-		glm::mat4 _viewProjection = glm::mat4(1.0f);
+		glm::mat4 m_viewProjection = glm::mat4(1.0f);
 
-		glm::vec3 _position = glm::vec3(0.0f, 0.0f, 0.0f);
-		glm::vec3 _forward = glm::vec3(0.0f, 0.0f, -1.0f);
-		glm::vec3 _up = glm::vec3(0.0f, 1.0f, 0.0f);
-		glm::vec3 _right = glm::vec3(1.0f, 0.0f, 0.0f);
+		glm::vec3 m_position = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 m_forward = glm::vec3(0.0f, 0.0f, -1.0f);
+		glm::vec3 m_up = glm::vec3(0.0f, 1.0f, 0.0f);
+		glm::vec3 m_right = glm::vec3(1.0f, 0.0f, 0.0f);
 
-		float _fovY = 50.0f;
-		float _aspectRatio = 16.0f / 9.0f;
-		float _nearPlane = 0.1f;
-		float _farPlane = 2000.0f;
+		float m_fovY = 50.0f;
+		float m_aspectRatio = 16.0f / 9.0f;
+		float m_nearPlane = 0.1f;
+		float m_farPlane = 2000.0f;
 
-		float _yaw = -90.0f;
-		float _pitch = 0.0f;
+		float m_yaw = -90.0f;
+		float m_pitch = 0.0f;
 
-		float _speed = 10.0f;
-		float _sensitivity = 0.3f;
+		float m_speed = 10.0f;
+		float m_sensitivity = 0.3f;
 
-		float _damping = 5.0f;
+		float m_damping = 5.0f;
 
-		bool _disableMouse = false;
+		bool m_disableMouse = false;
 
 	private:
-		float _targetYaw = 0.0f;
-		float _targetPitch = 0.0f;
+		float m_targetYaw = 0.0f;
+		float m_targetPitch = 0.0f;
 
-		glm::vec3 _worldUp;
-		glm::vec3 _targetPosition;
+		glm::vec3 m_worldUp;
+		glm::vec3 m_targetPosition;
 
 	public:
 		~Camera();
 
 		Camera(glm::vec3 position, glm::vec3 forward = glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f));
 
-		void update(float deltatime);
+		void Update(float deltatime);
 
-		void updateMouse(float deltaX, float deltaY);
-		void updateKey(float deltaTime, CameraMovement direction);
+		void UpdateMouse(float deltaX, float deltaY);
+		void UpdateKey(float deltaTime, CameraMovement direction);
 
-		void setPerspective(float fov, float aspectRatio, float nearPlane, float farPlane);
+		void SetPerspective(float fov, float aspectRatio, float nearPlane, float farPlane);
 
 	private:
-		void updateView();
+		void UpdateView();
 
-		glm::mat4 lookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp);
+		glm::mat4 LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp);
 
 	};
 }
